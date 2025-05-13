2ac3ce83237ff1e4ea75dc7169c9cf1bedb1456b
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-13 12:49:46 +0530

    Low-level design: Decorator pattern end_state
    Add decorator abstract class
    Add concrete decorator class to add to the existing behavior

--- src/main/java/com/mkr/oodpatterns/structural/DecoratorDemo.java
+++ src/main/java/com/mkr/oodpatterns/structural/DecoratorDemo.java
@@ -8,7 +8,33 @@ interface User {
 class BasicUser implements User {
     @Override
     public void printPrivileges() {
-        System.out.println("Basic User: Can login");
+        System.out.println("Basic User feature: Can login");
+    }
+}
+
+abstract class UserDecorator implements User {
+    private final User user;
+
+    public UserDecorator(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public void printPrivileges() {
+        user.printPrivileges();
+    }
+}
+
+class AdminUser extends UserDecorator {
+
+    public AdminUser(User user) {
+        super(user);
+    }
+
+    @Override
+    public void printPrivileges() {
+        super.printPrivileges();
+        System.out.println("Admin User feature: Can manage users");
     }
 }
 
@@ -16,5 +42,9 @@ public class DecoratorDemo {
     public static void main(String[] args) {
         User user = new BasicUser();
         user.printPrivileges();
+
+        User adminUser = new AdminUser(user);
+        System.out.println("Adding admin privileges:");
+        adminUser.printPrivileges();
     }
 }
