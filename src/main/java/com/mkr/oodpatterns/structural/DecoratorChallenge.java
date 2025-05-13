2a1543aa259b3dfc27d1f1cc6b67977d1b4fcabd
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-13 13:06:30 +0530

    Low-level design:
    Decorator pattern challenge - end_state
    
    decorate base room with carpet, chandelier

--- src/main/java/com/mkr/oodpatterns/structural/DecoratorChallenge.java
+++ src/main/java/com/mkr/oodpatterns/structural/DecoratorChallenge.java
@@ -1,19 +1,64 @@
 package com.mkr.oodpatterns.structural;
 
 interface Room {
-    void printFurniture();
+    void printDecoration();
 }
 
 class BasicRoom implements Room {
     @Override
-    public void printFurniture() {
+    public void printDecoration() {
         System.out.println("Basic room furniture: Bed, Table");
+    }
+}
+
+abstract class RoomDecorator implements Room {
+    protected final Room room;
+
+    public RoomDecorator(Room room) {
+        this.room = room;
+    }
+
+    @Override
+    public void printDecoration() {
+        System.out.println("Decorating room with: ");
+    }
+}
+
+class RoomWithCarpet extends RoomDecorator {
+
+    public RoomWithCarpet(Room room) {
+        super(room);
+    }
+
+    @Override
+    public void printDecoration() {
+        super.printDecoration();
+        System.out.println("Adding carpet");
+    }
+}
+
+class RoomWithChandelier extends RoomDecorator {
+
+    public RoomWithChandelier(Room room) {
+        super(room);
+    }
+
+    @Override
+    public void printDecoration() {
+        super.printDecoration();
+        System.out.println("Adding chandelier");
     }
 }
 
 public class DecoratorChallenge {
     public static void main(String[] args) {
         Room room = new BasicRoom();
-        room.printFurniture();
+        room.printDecoration();
+
+        Room carpetRoom = new RoomWithCarpet(room);
+        carpetRoom.printDecoration();
+
+        Room chandelierRoom = new RoomWithChandelier(new RoomWithCarpet(room));
+        chandelierRoom.printDecoration();
     }
 }
