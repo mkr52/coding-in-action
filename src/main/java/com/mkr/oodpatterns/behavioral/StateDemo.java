49bd9bbd94452837e8147cc139c89b32bf54ac40
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-21 10:57:35 +0530

    Behavioral pattern
    State pattern implementation

--- src/main/java/com/mkr/oodpatterns/behavioral/StateDemo.java
+++ src/main/java/com/mkr/oodpatterns/behavioral/StateDemo.java
@@ -1,48 +1,89 @@
 package com.mkr.oodpatterns.behavioral;
 
 class Auction {
-    private boolean isOpen;
+    private AuctionState state;
 
     public Auction() {
-        this.isOpen = false;
+        this.state = new AuctionClosed();
     }
 
     public void startAuction() {
-        if(isOpen) {
-            System.out.println("Auction is already open.");
-        } else {
-            isOpen = true;
-            System.out.println("Auction started.");
-        }
+        state.startAuction(this);
     }
 
     public void closeAuction() {
-        if(!isOpen) {
-            System.out.println("Auction is already closed.");
-        } else {
-            isOpen = false;
-            System.out.println("Auction closed.");
-        }
+        state.closeAuction(this);
     }
 
     public void placeBid() {
-        if(!isOpen) {
-            System.out.println("Bid can't be placed. Auction is already closed.");
-        } else {
-            System.out.println("Bid placed.");
-        }
+        state.placeBid();
     }
 
     public void closeBid() {
-        if(!isOpen) {
-            System.out.println("Bid can't be closed. Auction is already closed.");
-        } else {
-            System.out.println("Bid closed.");
-        }
+        state.closeBid();
     }
 
-
+    public void setState(AuctionState state) {
+        this.state = state;
+    }
 }
+
+interface AuctionState {
+    // define methods that depend on the state
+    void startAuction(Auction auction);
+    void closeAuction(Auction auction);
+    void placeBid();
+    void closeBid();
+}
+
+class AuctionStarted implements AuctionState {
+
+    @Override
+    public void startAuction(Auction auction) {
+        System.out.println("Auction is already started.");
+    }
+
+    @Override
+    public void closeAuction(Auction auction) {
+        auction.setState(new AuctionClosed());
+        System.out.println("Auction closed.");
+    }
+
+    @Override
+    public void placeBid() {
+        System.out.println("Bid placed.");
+    }
+
+    @Override
+    public void closeBid() {
+        System.out.println("Bid closed.");
+    }
+}
+
+class AuctionClosed implements AuctionState {
+
+    @Override
+    public void startAuction(Auction auction) {
+        auction.setState(new AuctionStarted());
+        System.out.println("Auction started");
+    }
+
+    @Override
+    public void closeAuction(Auction auction) {
+        System.out.println("Auction is already closed.");
+    }
+
+    @Override
+    public void placeBid() {
+        System.out.println("Bid can't be placed. Auction is already closed.");
+    }
+
+    @Override
+    public void closeBid() {
+        System.out.println("Bid can't be closed. Auction is already closed.");
+    }
+}
+
 public class StateDemo {
 
     public static void main(String[] args) {
@@ -52,6 +93,8 @@ public class StateDemo {
 
         auction.startAuction();
 
+        auction.placeBid();
+        auction.closeBid();
         auction.closeAuction();
         auction.closeBid();
 
