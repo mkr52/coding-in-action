ac6f078443bb1ddae76632738c27e07609ddf9ea
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-06-16 10:39:38 +0530

    quick sort implementation

--- src/main/java/com/mkr/dsa_striver/sorting/QuickSort.java
+++ src/main/java/com/mkr/dsa_striver/sorting/QuickSort.java
@@ -2,13 +2,18 @@ package com.mkr.dsa_striver.sorting;
 
 import java.util.Arrays;
 
+// TC(Best case): O(n log n); for out of order array
+// TC(Worst case): O(n^2); for already sorted array; better to use merge sort or insertion sort for such cases
 public class QuickSort {
 
     public static void main(String[] args) {
         int[] arr = {4, 6, 1, 7, 3, 2, 5};
-        int returnedIndex = pivot(arr, 0, arr.length - 1);
-        System.out.println("Pivot index returned: " + returnedIndex);
-        System.out.println("Array after pivoting: " + Arrays.toString(arr));
+        System.out.println("Original array: " + Arrays.toString(arr));
+//        int returnedIndex = pivot(arr, 0, arr.length - 1);
+        quickSort(arr);
+//        System.out.println("Pivot index returned: " + returnedIndex);
+//        System.out.println("Array after pivoting: " + Arrays.toString(arr));
+        System.out.println("Array after sorting: " + Arrays.toString(arr));
     }
 
     // Pivot
@@ -22,6 +27,23 @@ public class QuickSort {
         }
         swap(arr, pivotIndex, swapIndex);
         return swapIndex;
+    }
+
+    // Quick sort helper
+    public static void quickSortHelper(int[] arr, int left, int right) {
+        // Base case: if the array is already sorted or has one element
+        if (left < right) {
+            // Find the pivot index
+            int pivotIndex = pivot(arr, left, right);
+
+            // Recursively sort left and right subarrays
+            quickSortHelper(arr, left, pivotIndex - 1);
+            quickSortHelper(arr, pivotIndex + 1, right);
+        }
+    }
+
+    public static void quickSort(int[] arr) {
+        quickSortHelper(arr, 0, arr.length - 1);
     }
 
     // Add common swap method
