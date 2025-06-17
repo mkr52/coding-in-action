d4e68d5f135de5e01d06f051fb63e2ef93fcd006
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-06-17 10:51:11 +0530

    queue constructor impl.

---
+++ src/main/java/com/mkr/dsa_striver/stacks_Q/Queues.java
@@ -0,0 +1,51 @@
+package com.mkr.dsa_striver.stacks_Q;
+
+public class Queues {
+
+    public static void main(String[] args) {
+        Queues queue = new Queues(5);
+        queue.getFront();
+        queue.getLast();
+        queue.getLength();
+        queue.printStack();
+    }
+
+    private Node front;
+    private Node last;
+    private int length;
+
+    static class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queues(int value) {
+        front = new Node(value);
+        last = front;
+        length = 1;
+    }
+
+    public void printStack() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getFront() {
+        System.out.println("Front: " + (front != null ? front.value : "Queue is empty"));
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
